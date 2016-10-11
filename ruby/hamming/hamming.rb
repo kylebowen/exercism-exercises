f183f5fac1209b82a4ff147f7e6b806a8809ad61
Kyle Bowen <kyle.m.bowen@gmail.com>
2016-10-10 20:23:39 -0700

    Add solution for hamming.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,16 @@ class Hamming
 
   def self.compute (a, b)
     return 0 if a == b
-    a.length
+    raise ArgumentError if a.length != b.length
+    find_mutations(a, b).count
   end
+end
+
+def find_mutations (a, b)
+  pairs = a.chars.zip(b.chars)
+  pairs.delete_if { |a, b| a == b }
+end
+
+module BookKeeping
+  VERSION = 3
 end
