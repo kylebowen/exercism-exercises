6c37fd8749356478463714ca25797efe26e08fe0
Kyle Bowen <kyle.m.bowen@gmail.com>
2016-10-10 22:45:34 -0700

    Further condense hamming.rb.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,8 @@
 class Hamming
 
-  def self.compute (strand1, strand2)
+  def self.compute strand1, strand2
     raise ArgumentError if strand1.length != strand2.length
-    mutations = strand1.chars.zip(strand2.chars)
-    mutations.delete_if { |nucleotide1, nucleotide2|
-      nucleotide1 == nucleotide2 }
-    mutations.count
+    strand1.chars.zip(strand2.chars).count { |n1, n2| n1 != n2 }
   end
 end
 
