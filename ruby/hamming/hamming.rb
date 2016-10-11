c48e87ebb84fc520d4557fc10c0d924d119961f1
Kyle Bowen <kyle.m.bowen@gmail.com>
2016-10-10 22:04:33 -0700

    Simplify hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,13 +3,10 @@ class Hamming
   def self.compute (a, b)
     return 0 if a == b
     raise ArgumentError if a.length != b.length
-    find_mutations(a, b).count
+    mutations = a.chars.zip(b.chars)
+    mutations.delete_if { |a, b| a == b }
+    mutations.count
   end
-end
-
-def find_mutations (a, b)
-  pairs = a.chars.zip(b.chars)
-  pairs.delete_if { |a, b| a == b }
 end
 
 module BookKeeping
