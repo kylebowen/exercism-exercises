d6916052450f88697279cdd5fece5e5b2aada9e4
Kyle Bowen <kyle.m.bowen@gmail.com>
2017-03-01 06:33:26 -0800

    Update solution for rna_transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,23 +1,8 @@
 class Complement
 
   def self.of_dna dna_strand
-    rna_strand = dna_strand.chars.map do |nucleotide|
-      substitute_complement(nucleotide)
-    end.join
-    rna_strand.length == dna_strand.length ? rna_strand : ''
-  end
-
-  def self.substitute_complement(nucleotide)
-    case nucleotide
-    when 'C'
-      'G'
-    when 'G'
-      'C'
-    when 'T'
-      'A'
-    when 'A'
-      'U'
-    end
+    return '' if dna_strand.match(/[^CGTA]/)
+    dna_strand.tr('CGTA', 'GCAU')
   end
 end
 
