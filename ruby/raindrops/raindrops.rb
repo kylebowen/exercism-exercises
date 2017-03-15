5173c3f010873907f28a2001b6382e0d1a5cd38d
Kyle Bowen <kyle.m.bowen@gmail.com>
2017-03-15 11:13:10 -0700

    Refactor raindrops to hash with #each

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,22 +1,32 @@
 class Raindrops
+  SUBSTITUTIONS = {3 => 'Pling',
+                   5 => 'Plang',
+                   7 => 'Plong'}
 
-  # Shameless green
   def self.convert(number)
-    drops = ''
-    if number % 3 == 0
-      drops << "Pling"
+    converted_value = ''
+    SUBSTITUTIONS.each do |key, value|
+      converted_value << value if number % key == 0
     end
-    if number % 5 == 0
-      drops << "Plang"
-    end
-    if number % 7 == 0
-      drops << "Plong"
-    end
-    if number % 3 != 0 && number % 5 != 0 && number % 7 != 0
-      drops << number.to_s
-    end
-    drops
+    converted_value.empty? ? number.to_s : converted_value
   end
+  # Shameless green
+  # def self.convert(number)
+  #   drops = ''
+  #   if number % 3 == 0
+  #     drops << "Pling"
+  #   end
+  #   if number % 5 == 0
+  #     drops << "Plang"
+  #   end
+  #   if number % 7 == 0
+  #     drops << "Plong"
+  #   end
+  #   if number % 3 != 0 && number % 5 != 0 && number % 7 != 0
+  #     drops << number.to_s
+  #   end
+  #   drops
+  # end
 end
 
 module BookKeeping
