768968b4307ac3ee559ae53c59b7ffda8cd89259
Kyle Bowen <kyle.m.bowen@gmail.com>
2017-03-15 11:43:45 -0700

    remove comments

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -10,23 +10,6 @@ class Raindrops
     end
     converted_value.empty? ? number.to_s : converted_value
   end
-  # Shameless green
-  # def self.convert(number)
-  #   drops = ''
-  #   if number % 3 == 0
-  #     drops << "Pling"
-  #   end
-  #   if number % 5 == 0
-  #     drops << "Plang"
-  #   end
-  #   if number % 7 == 0
-  #     drops << "Plong"
-  #   end
-  #   if number % 3 != 0 && number % 5 != 0 && number % 7 != 0
-  #     drops << number.to_s
-  #   end
-  #   drops
-  # end
 end
 
 module BookKeeping
