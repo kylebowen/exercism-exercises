eb6fd672ae78c66491a6a562067415ae5268225e
Kyle Bowen <kyle.m.bowen@gmail.com>
2023-12-23 18:30:59 -0700

    Update go/party-robot to use composed functions

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -17,13 +17,18 @@ func HappyBirthday(name string, age int) string {
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	welcome := Welcome(name)
+	return strings.Join([]string{Welcome(name), tableInfo(table, direction, distance), neighborIntro(neighbor)}, "\n")
+}
 
+// tableInfo describes the guest's assigned table and relevant directions to reach it.
+func tableInfo(table int, direction string, distance float64) string {
 	tableAssignMsg := fmt.Sprintf("You have been assigned to table %03d.", table)
 	tableDirections := fmt.Sprintf("Your table is %s, exactly %.1f meters from here.", direction, distance)
-	tableInfo := strings.Join([]string{tableAssignMsg, tableDirections}, " ")
 
-	neighborIntro := fmt.Sprintf("You will be sitting next to %s.", neighbor)
+	return strings.Join([]string{tableAssignMsg, tableDirections}, " ")
+}
 
-	return strings.Join([]string{welcome, tableInfo, neighborIntro}, "\n")
+// neighborIntro informs a guest who they will be sitting next to.
+func neighborIntro(neighbor string) string {
+	return fmt.Sprintf("You will be sitting next to %s.", neighbor)
 }
