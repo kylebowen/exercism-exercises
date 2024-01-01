fb436a2033880215913232abc23eb1f22adf1249
Kyle Bowen <kyle.m.bowen@gmail.com>
2023-12-31 21:27:48 -0700

    Update solution for go/elons-toys

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Drive updates the meters driven based on the car's speed and reduces the battery based on the battery drainage.
 func (c *Car) Drive() {
-	if c.battery-c.batteryDrain > 0 {
+	if c.battery >= c.batteryDrain {
 		c.battery -= c.batteryDrain
 		c.distance += c.speed
 	}
@@ -12,12 +12,12 @@ func (c *Car) Drive() {
 
 // DisplayDistance returns the distance traveled for display.
 func (c Car) DisplayDistance() string {
-	return fmt.Sprintf("Driven %v meters", c.distance)
+	return fmt.Sprintf("Driven %d meters", c.distance)
 }
 
 // DisplayBattery returns the battery percentage for display.
 func (c Car) DisplayBattery() string {
-	return fmt.Sprintf("Battery at %v%%", c.battery)
+	return fmt.Sprintf("Battery at %d%%", c.battery)
 }
 
 // CanFinish calculates if a car has enough battery left to finish a track.
