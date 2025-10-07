07152e9eb1262c18effb9c829a58b516c060f133
Kyle Bowen <kyle.m.bowen@gmail.com>
2025-10-07 10:54:14 -0600

    Swap outdated comments for type info

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,23 +1,23 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  @spec expected_minutes_in_oven() :: integer
   def expected_minutes_in_oven(), do: 40
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  @spec remaining_minutes_in_oven(elapsed_minutes_in_oven :: integer) :: integer
   def remaining_minutes_in_oven(elapsed_minutes_in_oven) do
     expected_minutes_in_oven() - elapsed_minutes_in_oven
   end
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  @spec preparation_time_in_minutes(layer_count :: integer) :: integer
   def preparation_time_in_minutes(layer_count) do
     minutes_per_layer = 2
     layer_count * minutes_per_layer
   end
 
-  # Please define the 'total_time_in_minutes/2' function
-  def total_time_in_minutes(layer_count, elapsed_minutes_in_oven) do
-    preparation_time_in_minutes(layer_count) + elapsed_minutes_in_oven
+  @spec total_time_in_minutes(layer_count :: integer, elapsed_minutes :: integer) :: integer
+  def total_time_in_minutes(layer_count, elapsed_minutes) do
+    preparation_time_in_minutes(layer_count) + elapsed_minutes
   end
 
-  # Please define the 'alarm/0' function
+  @spec alarm() :: String.t()
   def alarm(), do: "Ding!"
 end
